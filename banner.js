c1c923ffd847cf8d2d68c29aee9f5a90a867a384
Boyoke Encok <mysqlproject98@gmail.com>
2025-02-10 19:20:36 +0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+export const banner = `
+            ${chalk.yellow('KITE AI  AUTO FARMING EXP')}                
+📢  ${chalk.yellow('Telegram Channel: https://t.me/mysqlproject')}`;
